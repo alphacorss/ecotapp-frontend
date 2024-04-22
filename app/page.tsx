744fa05fce54ce7481e2e2f916179b29642203bf
Prosper Noya <piinoya@gmail.com>
2024-04-22 12:16:00 -0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,10 @@ const Landing = () => {
   React.useEffect(() => {
     router.replace('/dashboard/home');
   }, [router]);
-  // const avaLinks = [
-  //   ...sideNavItems,
-  //   { title: 'Login', href: '/auth/login' },
-  //   { title: 'Forgot Password', href: '/auth/forgot-password' },
-  //   { title: 'Reset Password', href: '/auth/reset-password' },
-  // ];
+
   return (
     <div className="flex gap-3 flex-wrap p-5">
       <PageLoader />
-
-      {/* {avaLinks.map((item, index) => {
-        return (
-          <Link
-            key={index}
-            href={item.href}
-            className="text-gray-600 hover:text-gray-800 hover:bg-gray-100  rounded-md transition duration-300 ease-in-out w-full text-left cursor-pointer border border-gray-200 p-2 rouded-[var(--rounded-md)] hover:border-gray-300"
-          >
-            {item.title}
-          </Link>
-        );
-      })} */}
     </div>
   );
 };
