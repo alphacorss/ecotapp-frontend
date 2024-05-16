01d624aa8a7ecb5d5c02738ba36c7e6497f6664e
Prosper Noya <piinoya@gmail.com>
2024-05-16 14:34:32 -0300

    updates to sentry

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,18 +1,22 @@
-import Head from 'next/head';
+import type { Metadata } from 'next';
 import React from 'react';
+
+export const meta: Metadata = {
+  title: 'Ecotapp - Authentication',
+  icons: [
+    {
+      rel: 'icon',
+      href: '/headers/lock.svg',
+      sizes: 'any',
+      url: '/headers/lock.svg',
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <React.Fragment>
-      <Head>
-        <link rel="icon" href="/headers/lock.svg" sizes="any" />
-        <title>Ecotapp - Authentication</title>
-      </Head>
-      <div>{children}</div>
-    </React.Fragment>
-  );
+  return <div>{children}</div>;
 }
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,21 @@
+import * as Sentry from '@sentry/nextjs';
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+    Sentry.init({
+      dsn: 'https://a48272a33cd1feee336d1542f1b85912@o4507255804461056.ingest.us.sentry.io/4507255805509632',
+      tracesSampleRate: 1,
+      debug: false,
+    });
+    return;
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+    Sentry.init({
+      dsn: 'https://a48272a33cd1feee336d1542f1b85912@o4507255804461056.ingest.us.sentry.io/4507255805509632',
+      tracesSampleRate: 1,
+      debug: false,
+    });
+    return;
   }
 }
