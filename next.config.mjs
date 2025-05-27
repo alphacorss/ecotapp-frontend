7a7b19532c912d55b16eef422979902125a48a3b
JC-Coder <josephchimezie2003@gmail.com>
2025-05-27 06:12:21 +0100

    chore: update certifications list

--- next.config.mjs
+++ next.config.mjs
@@ -17,17 +17,14 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    org: "pii",
-    project: "javascript-nextjs",
-    silent: !process.env.CI,
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    tunnelRoute: "/monitoring",
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  }
-);
+export default withSentryConfig(nextConfig, {
+  org: 'pii',
+  project: 'javascript-nextjs',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  transpileClientSDK: true,
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
